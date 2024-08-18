c5e002b7829dd5726adda70d9ab7e1ec1ea0c9b5
Antonio Ruiz <antonio.ruiz.developer@gmail.com>
2024-08-18 16:39:26 +0200

    add tag selector

--- pages/workouts.tsx
+++ pages/workouts.tsx
@@ -1,19 +1,36 @@
 import { InferGetServerSidePropsType } from "next/types";
 import { NotionService } from "services/notion";
-import React, {  useState } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 
 export default function Workouts({
   items,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const selectedWorkout = items[selectedIndex];
+  const [selectedTag, setSelectedTag] = useState("");
+  const filteredItems = useMemo(() => {
+    if (!selectedTag) {
+      return items;
+    }
+    return items.filter((e) => e.tags.includes(selectedTag));
+  }, [selectedTag, items]);
+  const selectedWorkout = filteredItems?.[selectedIndex];
+
+  const uniqueTags = useMemo(() => {
+    const allTagsFlatten = items.map((e) => e.tags).flat();
+    return [...new Set(allTagsFlatten)];
+  }, [items]);
+
+  useEffect(() => {
+    setSelectedIndex(0);
+  }, [selectedTag]);
+
   return (
     <div className="min-h-screen bg-blue-900 flex flex-col justify-between p-4">
       <h1 className="text-3xl text-white text-center">
-        {selectedWorkout.name}
+        {selectedWorkout?.name}
       </h1>
 
-      <WorkoutItem workout={selectedWorkout} />
+      {selectedWorkout && <WorkoutItem workout={selectedWorkout} />}
       <div className="flex justify-between">
         {selectedIndex > 0 && (
           <button
@@ -23,7 +40,21 @@ export default function Workouts({
             Anterior
           </button>
         )}
-        {selectedIndex < items.length - 1 && (
+
+        <select
+          value={selectedTag}
+          onChange={(e) => setSelectedTag(e.target.value)}
+          className="bg-white text-black rounded-lg h-8 pl-2 shadow hover:bg-gray-100 ml-auto mr-auto"
+        >
+          <option value="">Todos ({items.length})</option>
+          {uniqueTags.map((tag) => (
+            <option key={tag} value={tag}>
+              {tag} ({items.filter((e) => e.tags.includes(tag)).length})
+            </option>
+          ))}
+        </select>
+
+        {selectedIndex < filteredItems.length - 1 && (
           <button
             onClick={() => setSelectedIndex(selectedIndex + 1)}
             className="bg-blue-600 text-lg text-white py-4 px-6 rounded-lg shadow hover:bg-blue-500 ml-auto"
