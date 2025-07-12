2b4037d2566dbd3b34b09b38785790f23c010569
Antonio Ruiz <antonio.ruiz.develop@gmail.com>
2025-07-12 23:11:50 +0200

    comment cronjob

--- pages/api/cron/daily.ts
+++ pages/api/cron/daily.ts
@@ -1,25 +1,26 @@
-import { NotionService } from "services/notion";
-import { TelegramService } from "services/telegram";
+// import { NotionService } from "services/notion";
+// import { TelegramService } from "services/telegram";
 
 export const config = {
   runtime: "edge",
 };
 
 export default async function handler() {
-  try {
-    const notion = new NotionService();
-    const telegram = new TelegramService();
+  console.log("Daily cron job is disabled");
+  // try {
+  //   const notion = new NotionService();
+  //   const telegram = new TelegramService();
 
-    if (!process.env.NOTION_DB_TASKS_ID)
-      return new Response("No database id", { status: 400 });
-    const data = await notion.getTodaysTasks();
-    const messages = data
-      .map((i, idx) => `${idx + 1}. ${i.title} -> ${i.assignedTo}`)
-      .join("\n");
-    const response = await telegram.sendMessage(encodeURIComponent(messages));
-    console.log(response);
-    return new Response("OK", { status: 200 });
-  } catch (error) {
-    return new Response(`Error: ${error}`, { status: 500 });
-  }
+  //   if (!process.env.NOTION_DB_TASKS_ID)
+  //     return new Response("No database id", { status: 400 });
+  //   const data = await notion.getTodaysTasks();
+  //   const messages = data
+  //     .map((i, idx) => `${idx + 1}. ${i.title} -> ${i.assignedTo}`)
+  //     .join("\n");
+  //   const response = await telegram.sendMessage(encodeURIComponent(messages));
+  //   console.log(response);
+  //   return new Response("OK", { status: 200 });
+  // } catch (error) {
+  //   return new Response(`Error: ${error}`, { status: 500 });
+  // }
 }
